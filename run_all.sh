dbdc012dc2e1cad8665e36485c25100a83267f23
Danelle Cline <dcline@mbari.org>
2018-01-31 21:44:34 -0800

    Revert "Valid config and run scripts from MBARI benthic tests"
    This reverts commit 1cc9925

--- run_all.sh
+++ run_all.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-set -x  
-function tmux_start_train { tmux new-window -d  -n "train:GPU$1" "${*:2}; exec bash"; }
-function tmux_start_test { tmux new-window -d  -n "test:CPU$1" "${*:2}; exec bash"; }
-IDX=$(seq 0 3)
-models=( ["0"]="faster_rcnn_resnet101_coco_100" ["1"]="faster_rcnn_resnet101_coco_300" \
-	 ["2"]="faster_rcnn_resnet101_coco_20" ["3"]="faster_rcnn_resnet101_coco_50" )
-tmux new-session -d -s "train" 
-for gpu_index in $IDX; do
-  tmux_start_train ${gpu_index} \
-  ./run.sh ${models[gpu_index]} train ${gpu_index} 
-done
-tmux new-session -d -s "test" 
-for index in $IDX; do
-  tmux_start_test ${index} \
-  ./run.sh ${models[index]} test
-done
-sleep 12h
-tmux kill-session -t train
-tmux kill-session -t test
+nohup ./run.sh faster_rcnn_resnet101_coco960540resolution_smallanchor train 0 > faster_rcnn_resnet101_coco960540resolution_smallanchor.out 2>&1 &
+nohup ./run.sh ssd_inception_v2_coco_600resolution_smallanchor train 1 > ssd_inception_v2_coco_600resolution_smallanchor.out  2>&1 &
+#nohup ./run.sh faster_rcnn_resnet101_coco960540resolution_smallanchor_meanadjusted train 1 > faster_rcnn_resnet101_coco960540resolution_smallanchor_meanadjusted.out 2>&1 &
+#nohup ./run.sh faster_rcnn_resnet101_coco960540resolution_smallanchor_contrastaug train 2 > faster_rcnn_resnet101_coco960540resolution_smallanchor_contrastaug.out 2>&1 &
