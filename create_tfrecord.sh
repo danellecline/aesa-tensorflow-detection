18ca36f4d0ecb44cdfeb7691f53d9754aa880735
Danelle Cline <dcline@mbari.org>
2018-02-08 16:57:14 -0800

    Combined records

--- create_tfrecord.sh
+++ create_tfrecord.sh
@@ -3,15 +3,15 @@ source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection/bin/activate
 cd ~/Dropbox/GitHub/aesa-tensorflow-detection/
 TOP_DIR='/Users/dcline/Dropbox/GitHub/aesa-tensorflow-detection/'
 python create_tfrecord.py \
---collection M56_1000x1000_by_group M56_1000x1000_by_group \
+--collection M56_1000x1000_by_group M535455_1000x1000_by_group \
 --data_dir $TOP_DIR/data/ \
---output_path $TOP_DIR/data/M56_1000x1000_train_by_group.record \
+--output_path $TOP_DIR/data/M53545556_1000x1000_train_by_group.record \
 --label_map_path $TOP_DIR/data/aesa_group_map.pbtxt \
 --set train
 
 python create_tfrecord.py \
---collection M56_1000x1000_by_group M56_1000x1000_by_group \
+--collection M56_1000x1000_by_group M535455_1000x1000_by_group \
 --data_dir $TOP_DIR/data/ \
---output_path $TOP_DIR/data/M56_1000x1000_test_by_group.record \
+--output_path $TOP_DIR/data/M53545556_1000x1000_test_by_group.record \
 --label_map_path $TOP_DIR/data/aesa_group_map.pbtxt \
 --set test
