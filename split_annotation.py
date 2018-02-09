3dd26e6e98fc65a7cdf236c09698ab95dba723a6
Danelle Cline <dcline@mbari.org>
2018-02-09 15:51:03 -0800

    Split isn't working on array so simplified to indexing into array directly; switched to larger train set

--- split_annotation.py
+++ split_annotation.py
@@ -3,13 +3,13 @@ import os
 import conf
 
 if __name__ == '__main__':
-
+    #collections = ['M535455_1000x1000_by_category', 'M56_1000x1000_by_category']
     collections = ['M535455_1000x1000_by_group', 'M56_1000x1000_by_group']
 
     for c in collections:
         dir = os.path.join(conf.DATA_DIR, c)
-        train_per = 0.5
-        tests_per = 0.5
+        train_per = 0.9
+        tests_per = 0.1
         utils.split(dir, train_per, tests_per)
 
     print('Done')
