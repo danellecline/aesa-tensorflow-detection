c14c9d4d3a907c2b4507082bb295387af58e1ac7
Danelle Cline <dcline@mbari.org>
2018-02-15 13:24:22 -0800

    Removed local venv reference

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-# Training command for executing on a DevBox
+#!/bin/bash
+# Training/testing command for executing models on a DevBox
 # Execute training with run_train_devbox.sh <model dir name> <test|train> <gpu device #>
 # e.g. run.sh faster_rcnn_resnet50_coco test 0
 # This will train model faster_rcnn_resnet50_coco on GPU device 0
 # best to split the train/test for each model on different GPUs which each have 12GB of memory
 # The models use a lot of memory during training, and can spike during
 # testing at checkpoints
-source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection-devbox/bin/activate
+source $TF_VENV/bin/activate
 pushd tensorflow_models/research
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim:`pwd`/object_detection
 popd
