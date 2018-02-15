9d2d1b0acf6d176efdfc62615a0395e2ca53baec
danellecline <dcline@mbari.org>
2018-02-15 12:46:25 -0800

    added description and removed local venv reference

--- run_inference.sh
+++ run_inference.sh
@@ -1,20 +1,37 @@
-#!/usr/bin/env bash
-# Run inference command on a DevBox
-# Execute inference with run_inference.sh <tfrecord> <model> <gpu device>
-# e.g. run_inference.sh M56_900x900_test_by_group.record faster_rcnn_resnet101_coco_100_smallanchor 0
-source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection-devbox/bin/activate
+#!bin/env bash
+# Run inference command on exported model 
+# Requires all models be exported previously with export_graphs_all.sh
+set -x
+source $TF_VENV/bin/activate
 pushd tensorflow_models/research
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim:`pwd`/object_detection
 popd
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/
+if [ "$3" == -1 ]
+then
+export CUDA_VISIBLE_DEVICES=""
+CSV_FILE=`pwd`/data/"$1"-"$2"-CPU-time.csv
+else
 export CUDA_VISIBLE_DEVICES="$3"
+CSV_FILE=`pwd`/data/"$1"-"$2"-GPU-time.csv
+fi
+TF_RECORD_FILE_IN=`pwd`/data/"$1"
+MODEL_DIR=`pwd`/models/export/"$2"/frozen_inference_graph.pb
+mkdir -p `pwd`/data/imagerecords/
 
-TF_RECORD_FILE="$1"
-MODEL_DIR=`pwd`//models/export/"$2"/frozen_inference_graph.pb
-
-echo python tensorflow_models/research/object_detection/inference/infer_detections.py \
-  --input_tfrecord_paths=$TF_RECORD_FILE \
-  --output_tfrecord_path=$TF_RECORD_FILE\.detections.record \
+if [ "$4" == "discard_image_pixels" ]; then
+TF_RECORD_FILE_OUT=`pwd`/data/imagerecords/"$1"-"$2"_detections.record
+python tensorflow_models/research/object_detection/inference/infer_detections.py \
+  --input_tfrecord_paths=$TF_RECORD_FILE_IN \
+  --output_tfrecord_path=$TF_RECORD_FILE_OUT \
+  --csv_path=$CSV_FILE \
+  --inference_graph=$MODEL_DIR \
+  --discard_image_pixels
+else
+TF_RECORD_FILE_OUT=`pwd`/data/imagerecords/"$1"-"$2"_detections_images.record
+python tensorflow_models/research/object_detection/inference/infer_detections.py \
+  --input_tfrecord_paths=$TF_RECORD_FILE_IN \
+  --output_tfrecord_path=$TF_RECORD_FILE_OUT \
+  --csv_path=$CSV_FILE \
   --inference_graph=$MODEL_DIR
-  
-#--discard_image_pixels
+fi
--- run_inference_all.sh
+++ run_inference_all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Run inference on all models.  Assumes models are exported using export_
+# Execute inference with run_inference.sh <tfrecord> <model> <gpu device> <discard_image_pixels|keep_image_pixels>
+# Discard image pixels when benchmarking the performance on your GPU
+# e.g. run_inference.sh INFERENCE_TEST.record faster_rcnn_resnet101_coco_300_smallanchor 0 keep_image_pixels
 set -x
 function tmux_start_inference { tmux new-window -d  -n "inference:GPU$1" "${*:2}; exec bash"; }
 
@@ -17,13 +21,15 @@ NUM_MODELS=8
 # Run inference across GPU last, using four models at a time
 for i in $(seq 0 4 $(($NUM_MODELS-1))); do
   tmux new-session -d -s "inference"
-  tmux_start_inference 0 ./run_inference.sh INFERENCE_TEST.record ${models[i]} 0 discard_image_pixels
-  tmux_start_inference 1 ./run_inference.sh INFERENCE_TEST.record ${models[i + 1]} 1 discard_image_pixels
+  tmux_start_inference 0 ./run_inference.sh INFERENCE_TEST.record ${models[i]} 2 discard_image_pixels
+  tmux_start_inference 1 ./run_inference.sh INFERENCE_TEST.record ${models[i + 1]} 3 discard_image_pixels
   tmux_start_inference 2 ./run_inference.sh INFERENCE_TEST.record ${models[i + 2]} 2 discard_image_pixels
   tmux_start_inference 3 ./run_inference.sh INFERENCE_TEST.record ${models[i + 3]} 3 discard_image_pixels
-  sleep 2m
+  sleep 5m
   tmux kill-session -t inference
 done
+./time_plot.sh
+exit
 
 # Run inference each across CPU  last
 for i in $(seq 0 1 $(($NUM_MODELS-1))); do
