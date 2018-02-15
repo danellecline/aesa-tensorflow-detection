63c884f9bff197d03411402981c3dd4e6e9581be
danellecline <dcline@mbari.org>
2018-02-15 12:36:27 -0800

    removed local venv reference

--- create_tfrecord.sh
+++ create_tfrecord.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection/bin/activate
-cd ~/Dropbox/GitHub/aesa-tensorflow-detection/
-TOP_DIR='/Users/dcline/Dropbox/GitHub/aesa-tensorflow-detection/'
+source $TF_VENV/bin/activate 
 python create_tfrecord.py \
 --collection M56_1000x1000_by_group M535455_1000x1000_by_group \
---data_dir $TOP_DIR/data/ \
---output_path $TOP_DIR/data/M53545556_1000x1000_train_by_group.record \
---label_map_path $TOP_DIR/data/aesa_group_map.pbtxt \
+--data_dir $PWD/data/ \
+--output_path $pWD/data/M53545556_1000x1000_train_by_group.record \
+--label_map_path $PWD/data/aesa_group_map.pbtxt \
 --set train
 
 python create_tfrecord.py \
 --collection M56_1000x1000_by_group M535455_1000x1000_by_group \
---data_dir $TOP_DIR/data/ \
---output_path $TOP_DIR/data/M53545556_1000x1000_test_by_group.record \
---label_map_path $TOP_DIR/data/aesa_group_map.pbtxt \
+--data_dir $PWD/data/ \
+--output_path $PWD/data/M53545556_1000x1000_test_by_group.record \
+--label_map_path $PWD/data/aesa_group_map.pbtxt \
 --set test
