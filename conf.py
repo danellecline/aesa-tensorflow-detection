1d13c4a7b9818b5682aeb001325668ff4c72b752
Danelle Cline <dcline@mbari.org>
2018-02-19 14:58:06 -0800

    fixed scaling

--- conf.py
+++ conf.py
@@ -6,11 +6,11 @@ OPTIMIZED_CATEGORIES = ['ENYPNIASTESEXIMIA', 'PENIAGONE', 'AMPERIMA']
 OPTIMIZED_GROUPS = ['HOLOTHUROIDEA', "ARTHOPODA', 'ASTEROIDEA"]
 OPTIMIZE_BOX = False
 
-TARGET_TILE_WIDTH = 600
-TARGET_TILE_HEIGHT = 600
+TARGET_TILE_WIDTH = 500
+TARGET_TILE_HEIGHT = 500
 LABEL_PATH_PATH = '{0}/data/aesa_group_map.pbtxt'.format(TOP_DIR)
 
-COLLECTION_M535455 =  'M535455_{0}x{1}_by_group'.format(TARGET_TILE_WIDTH, TARGET_TILE_HEIGHT)
+'''COLLECTION_M535455 =  'M535455_{0}x{1}_by_group'.format(TARGET_TILE_WIDTH, TARGET_TILE_HEIGHT)
 COLLECTION = COLLECTION_M535455
 ANNOTATION_DIR = '{0}/{1}/Annotations/'.format(DATA_DIR, COLLECTION)
 PNG_DIR = '{0}/{1}/PNGImages'.format(DATA_DIR, COLLECTION)
@@ -19,10 +19,9 @@ GROUP_FILE = None
 CLEAN_FILE = '{0}/annotations/M535455_crops_JMD.csv'.format(DATA_DIR)
 TILE_DIR = '/Volumes/ScratchDrive/AESA/M535455_tiles/'
 TILE_PNG_DIR = '/Volumes/ScratchDrive/AESA/M535455_10_1'
-FILE_FORMAT = '%s.jpg'
+FILE_FORMAT = '%s.jpg'''
 
-COLLECTION_M56 =  'M56_{0}x{1}_by_group'.format(TARGET_TILE_WIDTH, TARGET_TILE_HEIGHT)
-'''
+COLLECTION_M56 =  'M56_{0}x{1}_by_group_0pad'.format(TARGET_TILE_WIDTH, TARGET_TILE_HEIGHT)
 COLLECTION = COLLECTION_M56
 ANNOTATION_DIR = '{0}/{1}/Annotations/'.format(DATA_DIR, COLLECTION)
 PNG_DIR = '{0}/{1}/PNGImages'.format(DATA_DIR, COLLECTION)
@@ -33,7 +32,7 @@ CLEAN_FILE = None
 TILE_DIR = '/Volumes/ScratchDrive/AESA/M56_tiles/raw/'
 TILE_PNG_DIR = '/Volumes/ScratchDrive/AESA/M56_10_1'
 #Alternative file prefix to use for calculating the associated frame the annotation is from, e.g. M56_10441297_%d.jpg'")
-FILE_FORMAT = 'M56_10441297_%s.jpg'''''
+FILE_FORMAT = 'M56_10441297_%s.jpg'
 
 # xml constants
 DATABASE='Unknown'
@@ -43,6 +42,6 @@ POSE = 'Unspecified'
 DIFFICULT = '0'
 
 # Annotation constants
-PAD_PERCENTAGE = 25
-MIN_CROP = 75
+PAD_PERCENTAGE = 0 #25
+MIN_CROP = 16 #
 SHOW_ANNOTATION = False
