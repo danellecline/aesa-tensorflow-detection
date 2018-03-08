cb51f2c9253bb192491585d707b24d19d5d2786e
Danelle Cline <dcline@mbari.org>
2018-03-08 11:49:25 -0800

    changed test/train split to 50%

--- split_annotation.py
+++ split_annotation.py
@@ -16,13 +16,12 @@ import os
 import conf
 
 if __name__ == '__main__':
-    #collections = ['M535455_1000x1000_by_category', 'M56_1000x1000_by_category']
-    collections = ['M535455_1000x1000_by_group', 'M56_1000x1000_by_group']
+    collections = ['M535455_500x500_by_group_0pad', 'M56_500x500_by_group_0pad']
 
     for c in collections:
         dir = os.path.join(conf.DATA_DIR, c)
-        train_per = 0.9
-        tests_per = 0.1
+        train_per = 0.5
+        tests_per = 0.5
         utils.split(dir, train_per, tests_per)
 
     print('Done')
