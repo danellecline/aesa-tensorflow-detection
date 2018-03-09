7c191be9ae8cd4278eeb5c9500ac7d63b471b166
Danelle Cline <dcline@mbari.org>
2018-03-09 08:45:45 -0800

    added category map

--- create_label_map.py
+++ create_label_map.py
@@ -13,13 +13,19 @@ Creates a label map for usin TF from a cvs file
 '''
 import pandas as pd
 import os
-data = '{0}/data/annotations/M56_categories_and_groups_v13.csv'.format(os.getcwd())
-df = pd.read_csv(data, sep=',')
-map_out = os.path.join(os.getcwd(), 'data', 'aesa_group_map.pbtxt')
-index = 1
-grouped = df.groupby('Group')
-with open(map_out, 'w') as out:
-  for name, group in grouped:
-    str = 'item\n{{\n id:{0}\n  name: {1}\n}}\n'.format(index, name.upper())
-    out.write(str)
-    index += 1
+
+def convert(df, groupby):
+  index = 1
+  grouped = df.groupby(groupby)
+  map_out = os.path.join(os.getcwd(), 'data', 'aesa_{0}_map.pbtxt'.format(groupby.lower()))
+  with open(map_out, 'w') as out:
+    for name, group in grouped:
+    	str = 'item\n{{\n id:{0}\n  name: {1}\n}}\n'.format(index, name.upper())
+    	out.write(str.upper())
+    	index += 1
+
+if __name__ == '__main__':
+  data = '{0}/data/annotations/M56_categories_and_groups_v13.csv'.format(os.getcwd())
+  df = pd.read_csv(data, sep=',')
+  convert(df, 'Group')
+  convert(df, 'Category')
